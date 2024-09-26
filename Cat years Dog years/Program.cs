af85887f7ec58aae8de241b1c50708d55b925bf6
lhlazaro <llazaro@arkusnexus.com>
2024-09-26 11:30:52 -0600

    init

---
+++ Cat years Dog years/Program.cs
@@ -0,0 +1,64 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int humanYears = 5;
+        if (humanYears <= 0)
+        {
+            throw new ArgumentException("Human years must be a positive integer.");
+        }
+        Console.WriteLine($"[{humanYears},{GetCatYears(humanYears)},{GetDogYears(humanYears)}]");
+        Console.WriteLine($"[{humanYears},{GetAnimalYears(humanYears,AnimalType.Cat)},{GetAnimalYears(humanYears,AnimalType.Dog)}]");
+    }
+
+    public static int GetCatYears(int humanYears)
+    {
+        if (humanYears == 1)
+        {
+            return 15;
+        }
+        else if (humanYears == 2)
+        {
+            return 24;
+        }
+        else
+        {
+            return 24 + (humanYears - 2) * 4;
+        }
+    }
+    public static int GetDogYears(int humanYears)
+    {
+        if (humanYears == 1)
+        {
+            return 15;
+        }
+        else if (humanYears == 2)
+        {
+            return 24;
+        }
+        else
+        {
+            return 24 + (humanYears - 2) * 5;
+        }
+    }
+    public enum AnimalType
+    {
+        Cat,
+        Dog
+    }
+    public static int GetAnimalYears(int humanYears, AnimalType animal)
+    {
+        if (humanYears == 1)
+        {
+            return 15;
+        }
+        else if (humanYears == 2)
+        {
+            return 24;
+        }
+        else
+        {
+            return 24 + (humanYears - 2) * (animal==AnimalType.Cat?4:5);
+        }
+    }
+}
